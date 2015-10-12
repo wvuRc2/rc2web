f373b346dd1ac4724c507a3cd97e3f6aadaa494b
Mark Lilback <mark@lilback.com>
2015-10-12 15:58:03 -0400

    added file upload button for each workspace

--- src/wspace-summary.js
+++ src/wspace-summary.js
@@ -1,7 +1,14 @@
 import {bindable} from 'aurelia-framework';
+import $ from 'jquery';
 
 export class wspaceSummary {
 	@bindable wspace = null;
+	
+	fileSelected(event, wspaceId) {
+		let file = this.$event.target.files[0];
+		console.log("got file " + file.name + " in ws " + this.wspace.id);
+	}
+	
 }
 
 
