8a8d01478fa0de113633908263950da5458cd037
Mark Lilback <mark@lilback.com>
2015-10-22 14:49:25 -0400

    basic websocket implemented

--- src/session.js
+++ src/session.js
@@ -21,6 +21,54 @@ export class session {
 									d.getElementById('editor'),
 									d.getElementById('console')
 									);
+		this.websocket = new WebSocket('ws://localhost:8088/ws/1', [this.state.loginToken]);
+		this.websocket.onopen = e => {
+			console.log('websocket open');
+			this.websocket.binaryType = 'arraybuffer';
+			$(".loading").css('display', 'none')
+		};
+		this.websocket.onerror = e => {
+			console.log("error:" + e);
+		};
+		this.websocket.onmessage = e => {
+			if (typeof e.data === "string")
+				this.handleTextMessage(e.data);
+			else
+				this.handleBinaryMessage(e.data);
+		};
+		this.websocket.onclose = e => {
+			console.log("websocket closed");
+		};
+	}
+	executeQuery() {
+		this.websocket.send('{"msg":"execute", "code":"rnorm(21)"}');
+	}
+	requestHelp() {
+		this.websocket.send('{"msg":"help","topic":"print"}')
+	}
+	
+	showResults(msg) {
+		var newtext = $('<div></div>').text(msg.string)
+		$("#console").append(newtext)
+	}
+	
+	handleTextMessage(jsonString) {
+		console.log("got:" + jsonString);
+		var msg = JSON.parse(jsonString);
+		switch(msg.msg) {
+			case "userid":
+				this.socketId = msg.socketId;
+				break;
+			case 'results':
+				this.showResults(msg);
+				break;
+			default:
+				console.log("unknown message");
+		}
+	}
+	
+	handleBinaryMessage(arrayBuffer) {
+		console.log("got binary data");
 	}
 }
 
