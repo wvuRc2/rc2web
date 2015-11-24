c0fca2b51fe3fcca1edbb06fa481d755ed30b934
Mark Lilback <mark@lilback.com>
2015-11-24 17:30:21 -0500

    displays fancier output based on queryid

--- src/session.js
+++ src/session.js
@@ -11,9 +11,11 @@ import Splitter from 'splitter';
 export class session {
 	constructor(state) {
 		this.state = state;
+		this.currentInput = '';
 	}
 
 	attached() {
+		let me = this
 		this.cm = CodeMirror.fromTextArea(this.cmTextArea);
 		let d =document;
 		this.splitter = new Splitter(
@@ -26,6 +28,7 @@ export class session {
 			console.log('websocket open');
 			this.websocket.binaryType = 'arraybuffer';
 			$(".loading").css('display', 'none')
+			this.timerId = setInterval(function() { me.websocket.send('{"msg":"keepAlive"}') }, 30000)
 		};
 		this.websocket.onerror = e => {
 			console.log("error:" + e);
@@ -38,8 +41,20 @@ export class session {
 		};
 		this.websocket.onclose = e => {
 			console.log("websocket closed");
+			clearTimeout(this.timerId);
+			this.timerId = undefined;
 		};
 	}
+	checkForReturnKey(e) {
+		if (e.keyCode === 13)
+			this.executeConsoleQuery();
+	}
+	
+	executeConsoleQuery() {
+		let msg = {"msg": "execute", "code":this.currentInput};
+		this.websocket.send(JSON.stringify(msg))
+	}
+	
 	executeQuery() {
 		this.websocket.send('{"msg":"execute", "code":"rnorm(121)"}');
 	}
@@ -47,9 +62,43 @@ export class session {
 		this.websocket.send('{"msg":"help","topic":"print"}')
 	}
 	
+	echoQuery(msg) {
+		var label = "";
+		if (msg.fileId > 0) {
+		} else {
+			label = msg.query;
+		}
+		var qrcode = "qr" + msg.queryId;
+		var query = '<div class="qgroup" id="q' + msg.queryId + '">' +
+			'<a data-toggle="collapse" href="#' + qrcode + '" aria-controls="' + qrcode + '">' +
+			'<span class="fa fa-caret-down fa-fw qtoggle"></span></a>' + label + 
+			'<div id="' + qrcode + '" class="collapse in qresults"></div></div>';
+		$("#results").append(query);
+		$('#' + qrcode).on('shown.bs.collapse', function() {
+			$('#q' + msg.queryId + ' a span').removeClass('fa-caret-right').addClass('fa-caret-down');
+		});
+		$('#' + qrcode).on('hidden.bs.collapse', function() {
+			$('#q' + msg.queryId + ' a span').removeClass('fa-caret-down').addClass('fa-caret-right');
+		});
+	}
+	
 	showResults(msg) {
-		var newtext = $('<div></div>').text(msg.string)
-		$("#console").append(newtext)
+		if (msg.string && msg.string.length > 0) {
+			var newtext = $('<div class="qresults"></div>').text(msg.string)
+			if (msg.queryId > 0) {
+				$("#qr" + msg.queryId).append(newtext);
+			} else {
+				$("#results").append(newtext)
+			}
+		}
+		if (msg.images && msg.images.length > 0) {
+			var newtext = '<div class="imageGroup">'
+			msg.images.forEach(img => {
+				newtext += '<span class="fa fa-file-image-o fa-2x" imgid="' + img.id + '"></span>'
+			})
+			newtext += '</div>'
+			$("#results").append(newtext)
+		}
 	}
 	
 	handleTextMessage(jsonString) {
@@ -58,6 +107,9 @@ export class session {
 		switch(msg.msg) {
 			case "userid":
 				this.socketId = msg.socketId;
+				break;
+			case "echo":
+				this.echoQuery(msg);
 				break;
 			case 'results':
 				this.showResults(msg);
